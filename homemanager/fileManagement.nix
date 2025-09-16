0eb597f7f1364915ef234d74e8d75373813780b6
AadithyanRaju <aadithyan75@gmail.com>
2025-09-16 20:10:46 +0530

    commit

--- homemanager/fileManagement.nix
+++ homemanager/fileManagement.nix
@@ -4,8 +4,9 @@
   home.activation.dotfileLinks = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p $HOME/.config
     ln -snf /home/aadithyan/.dotfiles/term/zshrc $HOME/.zshrc
-    ln -snf /home/aadithyan/.dotfiles/hypr $HOME/.config/hypr
-    ln -snf /home/aadithyan/.dotfiles/waybar $HOME/.config/waybar
+    ln -snf /home/aadithyan/.dotfiles/config/hypr $HOME/.config/hypr
+    ln -snf /home/aadithyan/.dotfiles/config/waybar $HOME/.config/waybar
+    ln -snf /home/aadithyan/.dotfiles/config/wofi $HOME/.config/wofi
   '';
   
 }
