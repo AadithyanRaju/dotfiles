26b5bf33b3273f78131c3a80b0918b2e30920283
AadithyanRaju <aadithyan75@gmail.com>
2025-10-25 12:43:18 +0530

    removed

--- system/hardware/amdgpu.nix
+++ system/hardware/amdgpu.nix
@@ -13,7 +13,6 @@ in
   config = mkIf cfg.enable {
     hardware.opengl = {
       enable = true;
-      driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [
         rocmPackages.clr.icd          # OpenCL runtime
