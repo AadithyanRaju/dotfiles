426f6bf4480aaa42fe6bc9486a4316e630cf9e5b
AadithyanRaju <aadithyan75@gmail.com>
2026-04-21 17:24:26 +0530

    blacklist hp_wmi

--- Scripts/Omen_Keyboard.sh
+++ Scripts/Omen_Keyboard.sh
@@ -3,3 +3,4 @@ echo "00FF00" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone0
 echo "0000FF" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone00
 echo "FFFFFF" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone03
 echo "FF0000" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone02
+echo "blacklist hp_wmi" | sudo tee /etc/modprobe.d/blacklist-hp-wmi.conf
