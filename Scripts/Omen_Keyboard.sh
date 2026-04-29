5a73eb56211cefbd151e844a1af7274cf80ceb50
AadithyanRaju <aadithyan75@gmail.com>
2026-04-29 12:12:51 +0530

    Omen KeyBoard Lighting setup

--- Scripts/Omen_Keyboard.sh
+++ Scripts/Omen_Keyboard.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-echo "00FF00" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone01
-echo "0000FF" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone00
-echo "FFFFFF" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone03
-echo "FF0000" | sudo tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone02
-echo "blacklist hp_wmi" | sudo tee /etc/modprobe.d/blacklist-hp-wmi.conf
+echo "00FF00" | tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone01
+echo "0000FF" | tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone00
+echo "FFFFFF" | tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone03
+echo "FF0000" | tee /sys/devices/platform/omen-rgb-keyboard/rgb_zones/zone02
+
+if [ ! -f /etc/modprobe.d/blacklist-hp-wmi.conf ]; then
+    echo "blacklist hp_wmi" | sudo tee /etc/modprobe.d/blacklist-hp-wmi.conf
+fi
